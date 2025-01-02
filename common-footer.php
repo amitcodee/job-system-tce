b38af51e9c8843995697c859ffaded5298634645
Amit Sharma <121919097+amitcodee@users.noreply.github.com>
2025-01-02 12:39:19 +0530

    rmm

--- common-footer.php
+++
@@ -1,9 +0,0 @@
-</body>
-
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Font Awesome JS -->
-<script src="main.js"></script>
-</body>
-
-</html>
