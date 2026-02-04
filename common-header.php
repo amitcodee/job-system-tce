88ee4110f6882752d1c0a2b44309813fc3bb7327
amit-techcadd-dev <gourav.carrer.techcadd@gmail.com>
2026-02-04 15:02:48 +0530

    Update common-header.php

--- common-header.php
+++ common-header.php
@@ -16,6 +16,7 @@
 
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<script src="https://cdn.tiny.cloud/1/5xliusgp4nerfnlfh5ao0o45fozqdxibc7nfkygy1c00uitl/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
